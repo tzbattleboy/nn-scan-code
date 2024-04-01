45939eb65a840b906f0d2f2c1e671ea36b37c12d
apichatt <apichatt@sourcode.co.th>
2024-04-01 17:25:04 +0700

    add index

---
+++ index.js
@@ -0,0 +1,19 @@
+var Client = require('electron-ssh2').Client;
+var conn = new Client();
+conn.on('ready', function() {
+  console.log('Client :: ready');
+  conn.shell(function(err, stream) {
+    if (err) throw err;
+    stream.on('close', function() {
+      console.log('Stream :: close');
+      conn.end();
+    }).on('data', function(data) {
+      console.log('OUTPUT: ' + data);
+    });
+    stream.end('ls -l\nexit\n');
+  });
+}).connect({
+  host: '192.168.100.100',
+  port: 22,
+  username: 'frylock'
+});
